a5fc607d9e87c872f9e112b95d4007626ea8b02b
Saiful082 <smsaifulislam082@gmail.com>
2023-11-07 19:27:16 +0600

    init

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO)
+        .then( () => {
+         console.log('connected to the mongodb');
+})
+        .catch( (error) => {
+            console.log(error);
+        });
+
+const app = express();
+
+app.listen(5000, () => {
+console.log('Server is running on port 5000');
+    }
+);  
