be4538c445bc518259ed72bea6ca65850af03ec0
Saiful082 <smsaifulislam082@gmail.com>
2023-11-08 21:55:27 +0600

    complete  sign in page funcanilaty

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandle } from "../utils/error.js";
-import  jwt  from "jsonwebtoken";
-
-
+import jwt from 'jsonwebtoken';
 
 
 export const signup = async (req, res, next) => {
