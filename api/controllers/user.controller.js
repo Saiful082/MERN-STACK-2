516e474474169bc57525c56d1500c91f3696c336
Saiful082 <smsaifulislam082@gmail.com>
2023-11-16 18:07:54 +0600

    Create get user listingd api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandle } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 export const test = (req, res) => {
   res.json({
@@ -57,4 +58,18 @@ export const signOut = async (req, res, next) => {
  } catch (error) {
   next(error);
  }
-}
+};
+
+export const getUserListings = async (req, res, next) => {
+
+  if(req.user.id == req.params.user.id) {
+    try {
+      const listings = await Listing.findById({userRef: req.params.id});
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next (errorHandle(401,'You can only view your own listings!'));
+  }
+};
